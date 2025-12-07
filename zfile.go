064296073a6102cf301230c89328806d3f6cc0db
Scott Hess <scott@doubleu.com>
2025-12-06 20:35:32 -0800

    Plain-text writer to start.

--- zfile.go
+++ zfile.go
@@ -1,1 +1,15 @@
 package zfile
+
+import (
+	"io"
+	"os"
+)
+
+func Create(path string) (io.WriteCloser, error) {
+	inner, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		return nil, err
+	}
+
+	return inner, nil
+}
--- zfile_test.go
+++ zfile_test.go
@@ -1,8 +1,39 @@
 package zfile
 
 import (
+	"io"
+	"os"
+	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestZfileRead(t *testing.T) {
+}
+
+func TestZfileWrite(t *testing.T) {
+	tmpDir := t.TempDir()
+
+	const testdata = "This is a test"
+
+	t.Run("plain", func(t *testing.T) {
+		path := filepath.Join(tmpDir, "file")
+
+		out, err := Create(path)
+		require.Nil(t, err)
+		defer out.Close()
+
+		_, err = io.WriteString(out, testdata)
+		require.Nil(t, err)
+
+		err = out.Close()
+		require.Nil(t, err)
+
+		data, err := os.ReadFile(path)
+		require.Nil(t, err)
+
+		assert.Equal(t, testdata, string(data))
+	})
 }
